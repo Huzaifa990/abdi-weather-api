494431a91166416b91145b1e1cafdb79330b9f22
Huzaifa990 <sultanhuzaifa0@gmail.com>
2023-08-03 21:10:32 +0500

    Pro Version

--- src/API/config.js
+++ src/API/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    apiName: "http://api.openweathermap.org/data/2.5/weather?",
-    key: "&units=metric&appid=bd5e378503939ddaee76f12ad7a97608"
+    apiName: "http://pro.openweathermap.org/data/2.5/weather?",
+    key: "&units=metric&appid=3e43c3d8b684e8223c23a336c725a765"
 }
 //3e43c3d8b684e8223c23a336c725a765
+//bd5e378503939ddaee76f12ad7a97608 -> Pro version
